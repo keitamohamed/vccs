a2a90db7b89ceca8ebc1ea6918dd0c7eaada74a9
Mohamed Keita <keitamohamed12@gmail.com>
2018-01-15 19:17:55 -0500

    Working on Admin stage, and controller to load data.

--- src/com/keita/vccs/controller/AdminController.java
+++ src/com/keita/vccs/controller/AdminController.java
@@ -1,12 +1,25 @@
 package com.keita.vccs.controller;
 
+import com.keita.vccs.blueprint.Class;
+import com.keita.vccs.blueprint.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.Pane;
 
+import javax.swing.text.TabableView;
+
 public class AdminController {
+
     @FXML private Button aHome, aNewUser;
     @FXML private Pane aHomePane, aNewUserPane;
+    @FXML private TabableView aUserTable;
+    @FXML private TableColumn<User, String> userID, name, email, phone, type;
+
+    private ObservableList<User> users = FXCollections.observableArrayList();
+    private ObservableList<Class> classes = FXCollections.observableArrayList();
 
     @FXML
     public void initialize() {
